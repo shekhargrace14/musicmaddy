1b536eee1d2f30de819f69faed9c23daa44e3ca1
shekhargrace14 <shekhargrace14@gmail.com>
2023-09-13 08:15:26 +0530

    changes in hero and tab responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import HeroBG from "../assets/img1.jpg"
 const Hero = () => {
   return (
-
-        <div className="container-fluid">
-            <div className="hero">
-                <div className="row background d-flex j-center a-center">
-                    <img src={HeroBG} alt="heroImage" className="background-image"/>
-                </div>
-                <div className="row text d-flex j-center a-center">
-                    <div className="column" >
-                        <h1>Welcome to Shalom Worship</h1>
-                        <button>About Us</button> <br/>
-                        <button>Login</button> <br/>
-                        <button>Sign Up</button> <br/>
-                    </div>
+      <div className="hero">
+        {/* <div className="container-fluid"> */}
+            <div className="row background d-flex j-center a-center">
+                <img src={HeroBG} alt="heroImage" className="background-image"/>
+            </div>
+            <div className="row text">
+                <div className="column" >
+                    <h1>Welcome to Shalom Worship</h1>
+                    <button>About Us</button> <br/>
+                    <button>Login</button> <br/>
+                    <button>Sign Up</button> <br/>
                 </div>
             </div>
-        </div>
+        {/* </div> */}
+    </div>
   );
 };
 
