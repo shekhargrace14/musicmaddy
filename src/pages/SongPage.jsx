1644d1c79bab64c45f233de5a9e5b269826dcd42
shekhargrace14 <shekhargrace14@gmail.com>
2023-10-27 23:39:09 +0530

    tabs introduced

--- src/pages/SongPage.jsx
+++ src/pages/SongPage.jsx
@@ -16,8 +16,39 @@ const SongPage = (props) => {
           </div>
         </div>
       </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='column'>
+            <button onClick="chords">Chords</button>
+            <button onClick="lyrics">Lyrics</button>
+            <button onClick="translation">Translation</button>
+          </div> 
+        </div>
+      </div>
+
+
+      <div className='container songpage' >
+        <div className='row d-flex j-between'>
+          <div className='column'id="chordsCol">
+            <p>Chords</p>
+            <p>G     D     C       Em</p>
+            <p>Bhawar ke beech me Tu kehta hai tham Ja Tu meri taakat Hai Tu hi mera imaan</p>
+          </div> 
+          <div className='column' id="LyricsCol">
+            <p>Chords</p>
+            <p>G     D     C       Em</p>
+            <p>Bhawar ke beech me Tu kehta hai tham Ja Tu meri taakat Hai Tu hi mera imaan</p>
+          </div> 
+          <div className='column' id="translationCol">
+            <p>Chords</p>
+            <p>G     D     C       Em</p>
+            <p>Bhawar ke beech me Tu kehta hai tham Ja Tu meri taakat Hai Tu hi mera imaan</p>
+          </div> 
+        </div>
+      </div>
     </div>
   )
 }
 
 export default SongPage
+
