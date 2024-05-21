b0f245d70e22564679f23d543e15bf35f5e7d6fd
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-05-21 11:05:52 +0000

    tailwind installed

--- components/main.js
+++
@@ -1,5 +0,0 @@
-fetch("./components/main.html")
-.then(response=>response.text())
-.then(data=>
-    document.querySelector("main").innerHTML= data 
-)
--- components/nav.js
+++
@@ -1,5 +0,0 @@
-fetch("./components/nav.html")
-.then(response=> response.text())
-.then(data=> 
-    document.querySelector("nav").innerHTML = data
-)
