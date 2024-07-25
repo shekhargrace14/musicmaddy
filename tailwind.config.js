73cb7d6028e1ba8c2cd3b753fa224a2c4f2a05b6
shekhargrace14 <shekhargrace14@gmail.com>
2024-07-25 15:30:03 +0530

    music mady

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        'skeletonLoading' : "skeletonLoading 1s linear infinite alternate"
+      },
+      keyframes:{
+        skeletonLoading : { 
+        '0%' :{ backgroundColor: 'hsl(200, 200, 70%)' } ,
+        '100%' :{ backgroundColor: 'hsl(200, 200, 95%)' } ,
+        }
+      }
+    },
   },
   plugins: [],
 }
